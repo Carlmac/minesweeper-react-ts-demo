17dfc4593e60bb38f40c03aa83e57ba895ccc646
carlmac <carlmac@163.com>
2021-05-15 15:01:04 +0800

    编写 Header, Body 样式;编写生成雷区的函数

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
+import NumberDisplay from "../NumberDisplay";
+import { generateCells } from "../../utils";
+import Button from "../Button";
 
 const App: React.FC = () => {
-  return <div className="App">Hello from App</div>;
+  const [cells, setCells] = useState(generateCells());
+
+  const renderCells = (): React.ReactNode => {
+    return cells.map((row, rowIndex) =>
+      row.map((cell, colIndex) => <Button key={`${rowIndex}-${colIndex}`} />)
+    );
+  };
+
+  return (
+    <div className="App">
+      <div className="Header">
+        <NumberDisplay value={0} />
+        <div className="Face">
+          <span role="img">😎</span>
+        </div>
+        <NumberDisplay value={23} />
+      </div>
+      <div className="Body">{renderCells()}</div>
+    </div>
+  );
 };
 
 export default App;
